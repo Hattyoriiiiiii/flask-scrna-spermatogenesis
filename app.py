83a01f7e4f735cc811d1bde35b38de0488037860
Hattyoriiiiiii <www.tatsuya92@gmail.com>
2023-07-24 00:22:50 +0900

    Added summary statistics visualization for average, median, and quantile gene expression.

--- app.py
+++ app.py
@@ -25,18 +25,46 @@ def index():
         filtered_data = data[data['symbol'].isin(input_genes)]
         if normalize:
             filtered_data.iloc[:, 1:] = filtered_data.iloc[:, 1:].apply(lambda x: (x - x.mean()) / x.std(), axis=1)
-        img = io.BytesIO()
+        
+        # Create the subplot layout
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+        
         for i in range(len(filtered_data)):
-            plt.plot(filtered_data.columns[1:], filtered_data.iloc[i, 1:])
-        plt.title('Gene Expression during spermatogenesis')
+            axes[0].plot(filtered_data.columns[1:], filtered_data.iloc[i, 1:])
+        axes[0].set_title('Gene Expression during spermatogenesis')
         if normalize:
-            plt.ylabel('Expression (z-score)')
+            axes[0].set_ylabel('Expression (z-score)')
         else:
-            plt.ylabel('Expression (TP10K)')
+            axes[0].set_ylabel('Expression (TP10K)')
         if len(filtered_data) <= 10:
-            # plt.legend(filtered_data['symbol'], loc='upper left')
-            plt.legend(filtered_data['symbol'], loc='best')
-        plt.xticks(rotation=30)
+            # axes[0].legend(filtered_data['symbol'], loc='upper left')
+            axes[0].legend(filtered_data['symbol'], loc='best')
+        axes[0].set_xticklabels(filtered_data.columns[1:], rotation=30)
+        
+        # average, median, and quantiles across all genes
+        numeric_columns = filtered_data.columns[1:]
+        average_expression = filtered_data[numeric_columns].mean()
+        median_expression = filtered_data[numeric_columns].median()
+        first_quantile = filtered_data[numeric_columns].quantile(0.25)
+        third_quantile = filtered_data[numeric_columns].quantile(0.75)
+
+        # plot the average expression
+        axes[1].plot(filtered_data.columns[1:], average_expression, label="Average")
+
+        # plot the median expression
+        axes[1].plot(filtered_data.columns[1:], median_expression, label="Median")
+
+        # plot quantiles
+        axes[1].plot(filtered_data.columns[1:], first_quantile, label="First quantile", linestyle='dashed')
+        axes[1].plot(filtered_data.columns[1:], third_quantile, label="Third quantile", linestyle='dashed')
+
+        axes[1].set_title('Summary Statistics of Gene Expression during spermatogenesis')
+        axes[1].set_ylabel('Expression')
+        axes[1].set_xticklabels(filtered_data.columns[1:], rotation=30)
+        axes[1].legend()
+        
+        img = io.BytesIO()
+        plt.tight_layout()
         plt.savefig(img, format='png')
         plt.close()
         img.seek(0)
